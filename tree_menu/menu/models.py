87f7b5ac014c2565532d8c643681769263be9022
leggongum <arkad.tihonin@gmail.com>
2024-02-01 22:08:04 +0300

    changes in app menu: create models Menu, MenuItem, register models in admin, create index view

--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    menu = models.ForeignKey(
+        Menu, blank=True, related_name='items', on_delete=models.CASCADE
+         )
+    parent = models.ForeignKey('self', null=True,
+                               blank=True, related_name='children',
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- tree_menu/menu/views.py
+++ tree_menu/menu/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    return render(request=request, 
+                    template_name = 'menu/index.html', 
+                    context={
+                        'menu_name': request.GET.get('menu_name', 'main_menu'), 
+                        'item': request.GET.get('item')
+                        })
